eb0e671873db5d093703fda584755f521aa7f52e
rebeccalindman <rebeccalindman@icloud.com>
2025-05-22 18:59:08 +0200

    removed issue from order types

--- shared/order.ts
+++ shared/order.ts
@@ -1,28 +1,25 @@
 // shared/order.ts
-import { Product } from "./product";
+
 // ✅ Used on the frontend and for request bodies
 export interface OrderInput {
-    userID: number;
-    status: string;
-    orderDate: Date;
-    estimatedTimeMinutes: number;
-    items: OrderItemInput[];
-  }
-  
-  export interface OrderItemInput {
-    productID: number;
-    quantity: number;
-  }
-  
-  // ✅ Used only on backend (or shared if needed in frontend after creation)
-  export interface Order extends OrderInput {
-    id: number;
-    items: OrderItem[];
-  }
-  
-  // Used for OrderList component
-  export interface OrderItem extends OrderItemInput {
-    orderID: number;
-    productName: string;
-    productPrice: number;
-  }
+  userID: number;
+  status: string;
+  orderDate: Date;
+  estimatedTimeMinutes: number;
+  items: OrderItemInput[];
+}
+
+export interface OrderItemInput {
+  productID: number;
+  quantity: number;
+}
+
+// ✅ Used only on backend (or shared if needed in frontend after creation)
+export interface Order extends OrderInput {
+  id: number;
+  items: OrderItem[];
+}
+
+export interface OrderItem extends OrderItemInput {
+  orderID: number;
+}
